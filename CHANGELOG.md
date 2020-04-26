f7e8e20cab0dea6e909cbc62802cf8e54f796990
ViewFromTheBox <jacob.martella@att.net>
2020-04-25 23:03:00 -0500

    Initial commit to new repo

---
+++ CHANGELOG.md
@@ -0,0 +1,35 @@
+# Changelog
+
+## 1.4.1
+- Fixed issue with `is_plugin_active` function not being called correctly.
+
+## 1.4
+- Added the JM Live Blog Gutenberg block.
+- Tested with WordPress 5.3.
+
+## 1.3.2
+- Fixed: Issue with submenu items in the admin menu not showing on hover.
+- Tested with WordPress 4.9.
+
+## 1.3
+- Added: Support to show live blog on pages as well as posts.
+- Added: Widget to show live blog in the sidebar of posts if desired.
+
+## 1.2
+- Fixed: Issue with posts being duplicated when hitting "New Updates" button.
+- Fixed: Titles with double quotes not showing up correctly.
+- Fixed: Issue with first post not being loaded correctly.
+- Fixed: Errors showing when all updates are deleted.
+- Tested with WordPress 4.8.
+
+## 1.1
+- Added: Ability to customize the color of the "New Updates" alerts banner.
+- Tested with WordPress 4.7.
+
+# 1.0.1
+- Fixed: Issue with the latest two updates having the same content after saving without adding a new field.
+- Fixed: Issue with paragraph breaks not showing.
+- Fixed: Issue with oembeds not showing as they should.
+
+## 1.0
+- Initial Release
---
+++ js/jm-live-blog-admin.js
@@ -0,0 +1,36 @@
+jQuery( document ).ready( function() {
+    jQuery( '#live-blog-add-row' ).on('click', function() {
+        var row = jQuery( '.live-blog-empty-row.screen-reader-text' );
+        row.addClass( 'new-row jm-live-blog-fields' );
+        row.removeClass( 'live-blog-empty-row screen-reader-text' );
+        row.insertBefore( '.jm-live-blog-fields:first' );
+        jQuery( '.new-row .new-field' ).attr( "disabled",false );
+        return false;
+    } );
+
+    jQuery( '.live-blog-remove-row' ).on( 'click', function() {
+        jQuery( this ).parents( 'table' ).remove();
+        return false;
+    } );
+    jQuery( '#live_blog_alert_color' ).wpColorPicker();
+    if ( jQuery( '#live_blog_show_widget').prop( 'checked' ) === true ) {
+        jQuery( '#jm-live-blog-widget-title-row' ).show();
+        jQuery( '#jm-live-blog-widget-description-row' ).show();
+    } else {
+        jQuery( '#jm-live-blog-widget-title-row' ).hide();
+        jQuery( '#jm-live-blog-widget-description-row' ).hide();
+    }
+
+    jQuery('input[type=radio][name=live_blog_show_widget]').change(function() {
+        if (this.value === 'yes') {
+            jQuery( '#jm-live-blog-widget-title-row' ).show();
+            jQuery( '#jm-live-blog-widget-description-row' ).show();
+        }
+        if (this.value === 'no') {
+            jQuery( '#jm-live-blog-widget-title-row' ).hide();
+            jQuery( '#jm-live-blog-widget-description-row' ).hide();
+        }
+    });
+
+} );
+
