8b012877fd824b7122d76b4d5e633499bb16ea6f
Jacob Martella <me@jacobmartella.com>
2023-01-21 15:56:51 -0600

    Started working on updating the block.
    Addresses #25 #26

---
+++ blocks/build/live-blog-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e272dd183696b487c1f2');
---
+++ blocks/src/live-blog-block/template.php
@@ -0,0 +1,3 @@
+<p <?php echo get_block_wrapper_attributes(); ?>>
+ <?php esc_html_e( 'Live Blog – hello from a dynamic block!', 'live-blog' ); ?>
+</p>
